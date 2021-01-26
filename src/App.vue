e1b0ebc5365f5c3500f4bba44fd6bdd3c5f15020
bmainolnovo9l <bmainolnovo9l@gmail.com>
2021-01-26 17:42:22 +0300

    save

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style lang="scss">
+@import "./style/style.scss";
+
+</style>
